9cd959a808fd54e580fd9412aef9441cb92f0857
AniketBambale <aniketbambale@gmail.com>
2022-09-24 16:42:42 +0530

    new commit

--- src/Program37/StringUpperlowercount.java
+++ src/Program37/StringUpperlowercount.java
@@ -8,7 +8,7 @@ public class StringUpperlowercount {
 		
 		int lower=0;
 		
-		for()
+	//	for()
 		
 
 	}
---
+++ src/UpdatedPush/UpdatePush1.java
@@ -0,0 +1,10 @@
+package UpdatedPush;
+
+public class UpdatePush1 {
+
+	public static void main(String[] args) {
+		System.out.println("ANiket");
+
+	}
+
+}
