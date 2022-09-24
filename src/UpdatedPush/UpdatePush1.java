b751a4dc363f1c3298d7b1d06ace37e018bd066e
AniketBambale <aniketbambale@gmail.com>
2022-09-24 16:54:42 +0530

    commit by Aniket

--- src/UpdatedPush/UpdatePush1.java
+++ src/UpdatedPush/UpdatePush1.java
@@ -3,7 +3,8 @@ package UpdatedPush;
 public class UpdatePush1 {
 
 	public static void main(String[] args) {
-		System.out.println("ANiket");
+		System.out.println("ANiket bambale");
+		//aniket
 
 	}
 
