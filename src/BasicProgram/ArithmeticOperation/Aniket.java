0fa6cd0c207d33ed289b53732a5216991b8602ce
AniketBambale <aniketbambale@gmail.com>
2022-09-25 13:15:58 +0530

    commit

---
+++ src/BasicProgram/ArithmeticOperation/Aniket.java
@@ -0,0 +1,12 @@
+package BasicProgram.ArithmeticOperation;
+
+public class Aniket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+
+		System.out.println("aniket");
+
+	}
+
+}
